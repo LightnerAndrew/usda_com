bcd5a1e1252e6899c6cbebaee95e3e276d2b96f5
LightnerAndrew <lightnera1@gmail.com>
2018-11-20 16:22:43 -0500

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This package wraps the USDA Commodity API for ease of use in python. ',
+    author='Andrew Lightner',
+    license='MIT',
+)
